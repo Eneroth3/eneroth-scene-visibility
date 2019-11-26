0a4b8a1a0c17e7780e5d595d8349488b9afa1df9
Eneroth3 <eneroth3@gmail.com>
2019-11-26 20:33:44 +0100

    Correct entity summary
    False positive on curves before.

--- src/ene_scene_visibility/entity_summary.rb
+++ src/ene_scene_visibility/entity_summary.rb
@@ -15,11 +15,11 @@ module Eneroth
       def self.summary(entities)
         classes = entities.map(&:class).uniq
         class_name = classes.first.to_s.split("::").last
-        curves = entities.select { |e| e.respond_to?(:curve) }.map(&:curve).uniq
+        curves = entities.map { |e| e.respond_to?(:curve) && e.curve }.uniq
 
         if (classes & [Sketchup::Group, Sketchup::ComponentInstance]).size == 2
           OB["entity_summary.group_component", count: entities.size]
-        elsif curves.size == 1
+        elsif curves.size == 1 && curves.first
           OB["entity_summary.curve"]
         elsif classes.size == 1
           OB["entity_summary.#{class_name}", count: entities.size]
