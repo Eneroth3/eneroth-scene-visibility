3c985b0af635643c36eb4a7dd81da59105316742
Eneroth3 <eneroth3@gmail.com>
2020-01-15 20:40:47 +0100

    Bump version number

--- src/ene_scene_visibility.rb
+++ src/ene_scene_visibility.rb
@@ -21,8 +21,8 @@ module Eneroth
 
     EXTENSION.creator     = "Eneroth"
     EXTENSION.description = "Control entity visibility on a per scene basis."
-    EXTENSION.version     = "1.0.0"
-    EXTENSION.copyright   = "2019, #{EXTENSION.creator}"
+    EXTENSION.version     = "1.0.1"
+    EXTENSION.copyright   = "2020, #{EXTENSION.creator}"
     Sketchup.register_extension(EXTENSION, true)
   end
 end
